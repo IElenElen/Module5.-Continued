dfb150dccf7e8f20a0a4561324f345b72f6bf5d9
Ilka <Ilka@LAPTOP-VAVOB6QC>
2024-07-29 17:52:40 +0200

    Add project files.

---
+++ Module5.  Continued/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Module5.__Continued
+{
+    public class Program
+    {
+        public const string FILE_NAME = @"C:\Users\Ilka\Desktop\.net\zakupy.csv";
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = File.AppendText(FILE_NAME))
+            {
+                sw.WriteLine("biały seler, 4");
+            }
+            var lines = File.ReadLines(FILE_NAME);
+        }
+    }
+}
